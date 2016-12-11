0f0d562a630242a0c9361809941ecf4d014494bc
henry fritz <henryamsterfritz@gmail.com>
2016-12-11 01:11:04 +0000

    firebase working, fix glitch with filter for pL on next commit

--- client/js/app.js
+++ client/js/app.js
@@ -1,19 +1,46 @@
 
   
+ var config = {
+    apiKey: "AIzaSyCBNzlAWE4xyJ9YdYRymo7WEgY1gxOVhcE",
+    authDomain: "fresh-inventory.firebaseapp.com",
+    databaseURL: "https://fresh-inventory.firebaseio.com",
+    storageBucket: "fresh-inventory.appspot.com",
+    messagingSenderId: "566408288574"
+  };
+  firebase.initializeApp(config);
+  
 var freshInventory = angular.module('freshInventory', ["firebase"]);
-freshInventory.controller("itemList", function ($scope, $firebaseObject){
-    var ref = firebase.database().ref();
-    $scope.itemList = firebase.database().ref().child("item-list/");
-    var syncObject = $firebaseObject(ref);
-    syncObject.$bindTo($scope, "itemList");
-    console.log($scope.itemList);
+freshInventory.controller("itemList", function ($scope, $firebaseObject, $firebaseArray){
+
+    var database = firebase.database();
+    var il = database.ref("item-list");
     
-    $scope.items = [
+    var obj = $firebaseObject(il);
+    obj.$bindTo($scope, "itemlist");
+    il.on('value', getData, errData);
+    
+    function getData(data){
+ 
+     console.log(data.val());
+     var itemlist = data.val();
+     var keys = Object.keys(itemlist);
+     console.log(keys);
+      //$scope.itemlist= data.val();
+    }
+     function errData(err){
+     console.log(err);  
+    }
+    
+ 
+    //var syncObject = $firebaseObject(ref);
+    //syncObject.$bindTo($scope, "itemList");
+    //dummy data until FB becomes readable
+    /*$scope.items = [
         { 'name': 'Bananas',
         'upc': 4011,
         'bulk': true,
         'picture': 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/Cavendish_Banana_DS.jpg/220px-Cavendish_Banana_DS.jpg',
-        'description': 'The fruits of the Cavendish bananas are eaten raw, used in baking, fruit salads, fruit compotes, and to complement foods. The outer skin is partially green when sold in food markets, and turns yellow when it ripens. As it ripens the starches turn to sugar making a sweeter fruit.',
+        'description': 'The fruits of the Cavendish bananas are eatn raw, used in baking, fruit salads, fruit compotes, and to complement foods. The outer skin is partially green when sold in food markets, and turns yellow when it ripens. As it ripens the starches turn to sugar making a sweeter fruit.',
         'currentPrice': 0.59,
         'sale' : true,
         'salePrice': 0.44,
@@ -40,5 +67,5 @@ freshInventory.controller("itemList", function ($scope, $firebaseObject){
         'tags': ['grapes', 'sale']
         }
         
-        ]
+        ]*/
 });
